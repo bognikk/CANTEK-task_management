d529cd4a839b5c03901f5e3b6c10b73cbaa76594
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-09-03 11:28:01 -0400

    connecting everything to app

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,49 @@ import categories from './components/assets/categories';
 
 import './App.css'
 
-function App() {
+const App: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [filteredTasks, setFilteredTasks] = useState<Task[]>([]);
+
+  // Function to add a new task
+  const handleAddTask = (taskData: Omit<Task, 'id'>) => {
+    const dueDate = new Date(taskData.dueDate);
+    dueDate.setHours(24); // Adjust dueDate to start of day in local time zone
+
+    const newTask: Task = {
+      id: new Date().getTime(), // Unique timestamp-based ID
+      ...taskData,
+      dueDate, // Set dueDate separately
+    };
+
+    setTasks([...tasks, newTask]);
+  };
+
+  // Function to delete a task by ID
+  const handleDeleteTask = (taskId: number) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  // Function to filter tasks by category
+  const handleCategoryFilter = (selectedCategory: string) => {
+    // If the selected category is empty (All Categories), display all tasks
+    if (selectedCategory === '') {
+      setFilteredTasks(tasks);
+    } else {
+      // Filter tasks by the selected category
+      const filteredTasks = tasks.filter((task) => task.category === selectedCategory);
+      setFilteredTasks(filteredTasks);
+    }
+  };
 
   return (
-    <>
-      <TaskForm onSubmit={function (data: { title: string; dueDate: Date; category: 'Work' | 'Personal' | 'School'; }): void {
-        throw new Error('Function not implemented.');
-      }} />
-      <TaskFilter onSelectCategory={function (category: string): void {
-        throw new Error('Function not implemented.');
-      }} categories={[]} />
-      <TaskList tasks={[]} onDelete={function (taskId: number): void {
-        throw new Error('Function not implemented.');
-      }} />
-
-    </>
-  )
-}
+    <div>
+      <h1>Task Manager</h1>
+      <TaskForm onSubmit={handleAddTask} />
+      <TaskFilter onSelectCategory={handleCategoryFilter} categories={categories} />
+      <TaskList tasks={filteredTasks} onDelete={handleDeleteTask} />
+    </div>
+  );
+};
 
 export default App
--- src/components/TaskFilter.tsx
+++ src/components/TaskFilter.tsx
@@ -1,11 +1,15 @@
+// TaskFilter.tsx
 import React, { ChangeEvent } from 'react';
 
 interface TaskFilterProps {
   onSelectCategory: (category: string) => void;
-  categories: string[];
+  categories: readonly [string, string, string]; // Declare categories as a readonly tuple
 }
 
 const TaskFilter: React.FC<TaskFilterProps> = ({ onSelectCategory, categories }) => {
+  // Convert readonly tuple to a mutable array using the spread operator
+  const categoriesArray = [...categories];
+
   const handleCategoryChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const selectedCategory = event.target.value;
     onSelectCategory(selectedCategory);
@@ -16,7 +20,7 @@ const TaskFilter: React.FC<TaskFilterProps> = ({ onSelectCategory, categories })
       <label htmlFor="categoryFilter">Filter by Category:</label>
       <select id="categoryFilter" onChange={handleCategoryChange}>
         <option value="">All Categories</option>
-        {categories.map((category) => (
+        {categoriesArray.map((category) => (
           <option key={category} value={category}>
             {category}
           </option>

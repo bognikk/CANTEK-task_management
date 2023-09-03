7da8819c03ff56047e82d87425df89f7314899b9
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-09-03 11:02:43 -0400

    task list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Task from './components/assets/Task';
 import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 import './App.css'
 
@@ -12,7 +13,9 @@ function App() {
       <TaskForm onSubmit={function (data: { title: string; dueDate: Date; category: 'Work' | 'Personal' | 'School'; }): void {
         throw new Error('Function not implemented.');
       }} />
-
+      <TaskList tasks={[]} onDelete={function (taskId: number): void {
+        throw new Error('Function not implemented.');
+      }} />
     </>
   )
 }
